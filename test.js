020eb32a8653a3e9ad26d8d59902255d6457e2e7
almustanir <126793005+almustanir@users.noreply.github.com>
2023-03-02 15:34:29 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,31 @@
+//maths method is an intrinsic objects that provide basic mathematics functunality and constant .
+// let x = 3.16;
+//ways of using math functions
+
+//1_____ x = Math.round(x)
+//2_____  x = Math.floor(x)  it will return the number of digit and ignore the decimal point
+//3_____ x = Math.ceil(x)  it round them up to the nearest integer
+//4_____ x = Math.pow(x, 2)  it multiply it by 2
+//5_____ x = Math.sqrt(x)
+//6_____ x = Math.abs(x)  it turns negative numbers into positive numbers
+// console.log(x)
+
+// let x = 3.19;
+// let y = 5;
+// let z = 9;
+
+// maximum = Math.max(x, y, z);
+// console.log(maximum)
+
+
+// mini = Math.min(x, y, z);
+// console.log(mini)
+
+ //window interface are used to talk to the browser so the DOM is the property of the window 
+
+ 
+
+
+
+
+
